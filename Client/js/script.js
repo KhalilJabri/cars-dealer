26a6fc9dfbfdd6fc811c92954723d438171a5812
khalil jabri <jabrikhalil869@gmail.com>
2024-03-23 18:38:48 +0100

    adding logos of cars in the home page

--- Client/js/script.js
+++ Client/js/script.js
@@ -10,6 +10,11 @@ function hideSidebar() {
     sidebar.style.display = 'none';
 }
 
+// Creating an infinite logo carousel with pure CSS
+
+let copy = document.querySelector(".logo-slide").cloneNode(true);
+document.querySelector('.logoContainer').appendChild(copy);
+
 // first image header 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -19,9 +24,58 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 });
 
+// scroll between popular cars button
+
+const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nulla ac turpis porta commodo eget sit amet sapien. Vivamus finibus nibh et sollicitudin pretium. In mollis erat non tellus.";
+
 let cars = [
-    {name: "", desc: "", speed: "", transmission: "", turbo: "", gas: ""},
-    {name: "", desc: "", speed: "", transmission: "", turbo: "", gas: ""},
-    {name: "", desc: "", speed: "", transmission: "", turbo: "", gas: ""},
-    {name: "", desc: "", speed: "", transmission: "", turbo: "", gas: ""},
+    {name: "Audi", desc: desc, speed: "220 Km/h", transmission: "Auto", turbo: "50 KM", gas: "HEV", price: "150 DT", img: "./image/audi.jpg"},
+    {name: "BMW", desc: desc, speed: "200 Km/h", transmission: "Manual", turbo: "100 KM", gas: "BEV", price: "130 DT", img: "./image/bmw.jpg"},
+    {name: "Hyundai", desc: desc, speed: "180 Km/h", transmission: "Manual", turbo: "150 KM", gas: "P", price: "100 DT", img: "./image/hyundai.jpg"},
+    {name: "Mercedes-Benz", desc: desc, speed: "120 Km/h", transmission: "Auto", turbo: "60 KM", gas: "BEV", price: "160 DT", img: "./image/pngfind.com-car-front-png-218600.png"},
 ]
+
+const nameCar = document.querySelector('.nameCar');
+const descrip = document.querySelector('.desc');
+const vitesseValue = document.querySelector('.vitesseValue');
+const turboValue = document.querySelector('.turboValue');
+const transmissionValue = document.querySelector('.transmissionValue');
+const gasValue = document.querySelector('.gasValue');
+const globalPriceValue = document.querySelector('.globalPriceValue');
+const carImage = document.querySelector('.carImage');
+
+let currentCar = 0;
+
+function previousCar() {
+    if(currentCar == 0) {
+        currentCar = cars.length -1;
+    }else {
+        currentCar -= 1;
+    }
+    nameCar.textContent = cars[currentCar].name;
+    descrip.textContent = cars[currentCar].desc;
+    vitesseValue.textContent = cars[currentCar].speed;
+    turboValue.textContent = cars[currentCar].transmission;
+    transmissionValue.textContent = cars[currentCar].turbo;
+    gasValue.textContent = cars[currentCar].gas;
+    globalPriceValue.textContent = cars[currentCar].price;
+    carImage.src = cars[currentCar].img;
+
+}
+
+function nextCar() {
+    if(currentCar == cars.length -1) {
+        currentCar = 0;
+    }else {
+        currentCar +=1;
+    }
+    nameCar.textContent = cars[currentCar].name;
+    descrip.textContent = cars[currentCar].desc;
+    vitesseValue.textContent = cars[currentCar].speed;
+    turboValue.textContent = cars[currentCar].transmission;
+    transmissionValue.textContent = cars[currentCar].turbo;
+    gasValue.textContent = cars[currentCar].gas;
+    globalPriceValue.textContent = cars[currentCar].price;
+    carImage.src = cars[currentCar].img;
+
+}
